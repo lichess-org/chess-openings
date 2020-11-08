a99df905c61d621aea11edb93fdae0b1def3a8c7
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-11-08 20:54:25 +0100

    add scalachess export script

---
+++ bin/to-scalachess.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import sys
+import itertools
+
+
+def main(argv):
+    name = "X"
+    for prefix in "abcde":
+        if f"{prefix}.tsv" in argv[1]:
+            name = prefix.upper()
+
+    print("package chess")
+    print("package opening")
+    print()
+    print("// format: off")
+    print(f"private[opening] object FullOpeningPart{name} {{")
+    print()
+    print("  def db: Vector[FullOpening] = Vector(")
+
+    for line in itertools.islice(open(argv[1]), 1, None):
+        cols = line.split("\t")
+        print(f"""new FullOpening("{cols[0]}", "{cols[1]}", "{cols[2]}"),""")
+
+    print("  )")
+    print("}")
+
+
+if __name__ == "__main__":
+    main(sys.argv)
