72b61c5608ddd787185276c69d83dd4e0bca8518
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-06-26 23:56:08 +0200

    repo renamed

--- bin/to-scalachess.py
+++ bin/to-scalachess.py
@@ -13,7 +13,7 @@ def main(argv):
     print("package chess")
     print("package opening")
     print()
-    print("// Generated from https://github.com/niklasf/eco")
+    print("// Generated from https://github.com/niklasf/chess-openings")
     print("// format: off")
     print(f"private[opening] object FullOpeningPart{name} {{")
     print()
